8e97d1cacfe73e1f40dca08f8b622ee48a83750f
Jordi <jordi.hermoso.up@gmail.com>
2020-06-15 15:51:58 +0800

    BugLogger 🐛: Date formatting, Form Validation and Alerts

--- bug-logger/src/components/App.js
+++ bug-logger/src/components/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container'
 import Table from 'react-bootstrap/Table'
+import Alert from 'react-bootstrap/Alert'
 import LogItem from './LogItem'
+import { AddLogItem } from './AddLogItem'
 
 const App = () => {
 
@@ -29,12 +31,52 @@ const App = () => {
 		},
 	])
 
+	const [alert, setAlert] = useState({
+		show: false,
+		message: "",
+		variant: "success"
+	})
+
+	function addItem(item) {
+		if (item.text == "" || item.user == "" || item.priority == "") {
+			showAlert("Please enter all fields", 'danger')
+			return
+		}
+
+		item._id = Math.floor(Math.random() * 90000) + 1000
+		item.created = new Date().toString()
+
+		setLogs([...logs, item])
+		showAlert("Log added :)")
+		console.log(item)
+	}
+
+	function deleteItem(_id) {
+		setLogs(logs.filter((item) => item._id !== _id))
+		showAlert("Log removed ❌", "danger")
+	}
+
+	function showAlert(message, variant = 'success', seconds = 3000) {
+		setAlert({
+			show: true,
+			message,
+			variant
+		})
+
+		setTimeout(() => {
+			setAlert({
+				show: false,
+				message: "",
+				variant: "success"
+			})
+		}, seconds)
+	}
+
 	return (
 		<div className='app'>
-			<h1>React Electron Boilerplate</h1>
-			<p>This is a simple boilerplate for using React with Electron</p>
-
 			<Container>
+				<AddLogItem addItem={addItem} />
+				{alert.show && <Alert variant={alert.variant}>{alert.message}</Alert>}
 				<Table>
 					<thead>
 						<tr>
@@ -46,7 +88,7 @@ const App = () => {
 					</thead>
 					<tbody>
 						{
-							logs.map((log) => <LogItem log={log} />)
+							logs.map((log) => <LogItem log={log} key={log._id} deleteItem={deleteItem} />)
 						}
 					</tbody>
 				</Table>
--- bug-logger/src/components/LogItem.js
+++ bug-logger/src/components/LogItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
 import Badge from 'react-bootstrap/Badge'
+import Moment from "react-moment";
 
 
-const LogItem = ({ log: { _id, user, text, created, priority } }) => {
+const LogItem = ({ log: { _id, user, text, created, priority }, deleteItem }) => {
 
     const setVariant = () => {
         if (priority === 'high') {
@@ -22,9 +23,11 @@ const LogItem = ({ log: { _id, user, text, created, priority } }) => {
             <td><Badge variant={setVariant()} className="p-2" style={{ textTransform: "capitalize" }}>{priority}</Badge></td>
             <td>{text}</td>
             <td>{user}</td>
-            <td>{created}</td>
             <td>
-                <Button variant="danger" size="sm">x</Button>
+                <Moment format="MMMM Do YYYY, h:mm:ss a">{new Date(created)}</Moment>
+            </td>
+            <td>
+                <Button variant="danger" size="sm" onClick={() => deleteItem(_id)}>x</Button>
             </td>
         </tr>
     )
